4fca137553927d3d1c98abb0a46fea01576fbd2a
Cosmicfasttravel <marcus.chumanov@gmail.com>
2026-03-10 15:45:00 -0500

    Delete src directory

--- src/GPU.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef BUMPERDETECTION_SETUPGPU_H
-#define BUMPERDETECTION_SETUPGPU_H
-#include <opencv2/opencv.hpp>
-
-namespace gpu {
-    std::string setupGPUBackend(cv::dnn::Net& net);
-} // gpu
-
-#endif //BUMPERDETECTION_SETUPGPU_H
